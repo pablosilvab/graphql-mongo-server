02c524970f1ff9c8a4b48ed9bb4220b1dcf7871f
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-04-27 12:34:33 -0400

    add graphiQL tool

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import graphqlHTTP from "express-graphql";
 
 const app = express();
+
+app.get('/', (req,res)=>{
+    res.json({
+        message: 'hello world'
+    })
+})
+
+const schema = {};
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}));
 
 app.listen(3000, ()=>{
     console.log('Server running on port: 3000');
